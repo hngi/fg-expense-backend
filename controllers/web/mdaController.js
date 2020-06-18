12294151105644a5e9f38c77cebf04b6087a6485
The-indigo <adepojuadeyemi11@gmail.com>
2020-06-18 11:12:12 +0100

    mda first commit

--- controllers/web/mdaController.js
+++ controllers/web/mdaController.js
@@ -1,4 +1,22 @@
-/**
- * export.method = req, res function
- * 
- */
+// create a controller method in the mdaController that will return all 
+//ministries and their twiiter handle,
+// all ministers and their twitter handle
+// one for ministries and their handle
+// another for ministers and their handle
+
+// create a controller method in the mdaController that will return all mdas 
+// and their twiiter handle, all mda heads and their twitter handle. 
+// use the model as guide.
+
+const MDA=require("../../models/MDA")
+
+  exports.getMinistries = (req, res,next)=>{
+ MDA.find({},'name tweet_handle').then((result)=>{
+if (!result){
+    return res.send({status:"false",message:"No Mda found"})
+}
+return res.send({status:"True",message:result})
+ }).catch((err)=>console.log(err))
+  }
+  //User.findOne({_id: userId}).select("-password")
+  
--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 //demo data
-var userController = require('../controllers/userController');
+//var userController = require('../controllers/userController');
+var mdaController = require('../controllers/web/mdaController');
+
 
 // demo route
-router.post('/users', userController.newUser);
+//router.post('/users', userController.newUser);
+router.get('/mda',mdaController.getMinistries)
 module.exports = router;
