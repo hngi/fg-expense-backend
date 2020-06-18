a28f9de52518db67163e8ea88341b08225cdd6dd
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-06-18 20:12:33 +0200

    feat: implement upvoting on comment api

--- routes/comments.js
+++ routes/comments.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var router = express.Router();
 //require comment controllers
-var projectsController = require('../controllers/comment/commentController');
+var projectsController = require("../controllers/comment/commentController");
 
 //POST - User can post comments by Name and Email (i.e '/comment')
-router.post('/', projectsController.postCommentByEmail);
+router.post("/", projectsController.postCommentByEmail);
+
+//POST - Upvote comment (i.e '/comment/:id/upvotes')
+router.post("/:id/upvotes", projectsController.upvoteComment);
 
 //POST - Flag comments (i.e '/comment/flag/:id')
-router.post('/flag/:id', projectsController.flagComment);
+router.post("/flag/:id", projectsController.flagComment);
 
 //GET - Hide all unflagged comments (i.e '/comment/unflagged')
-router.get('/unflagged', projectsController.hideFlaggedComments);
+router.get("/unflagged", projectsController.hideFlaggedComments);
 
 module.exports = router;
