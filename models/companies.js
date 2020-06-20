c16db33ad13dc4d46c85b0c5957236abd7820d62
Dave <callremz@gmail.com>
2020-06-20 10:39:04 +0100

    fix: fix lint errors

--- models/companies.js
+++ models/companies.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CompanySchema = new Schema(
@@ -18,4 +18,4 @@ const CompanySchema = new Schema(
   }
 );
 
-module.exports = mongoose.model('companies', CompanySchema);
+module.exports = mongoose.model("companies", CompanySchema);
