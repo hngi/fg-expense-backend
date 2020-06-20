d3a2d269c7895e3eda850f0f65c9e13137e70432
AK <mywork304@gmail.com>
2020-06-20 22:55:48 +0200

    before

--- controllers/web/sectorController.js
+++ controllers/web/sectorController.js
@@ -2,26 +2,29 @@ var mongoose = require("mongoose");
 const sector=require('../../models/Sector')
 var getASector=async function(req,res){
     const id=req.params.id;
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        res.status(400).json({
-            msg:'id is required'
-        })
-    }
-    try {
-        const fsector=await sector.findById(id).populate('MAD').populate('Budget').populate('Payment')
-        if(fsector){
-            res.json(fsector)
-        }else{
-            res.json({
-                msg:'no sector found'
-            })
-        }
+    var sectors=await sector.find()
+    console.log(sectors)
+
+    // if(!mongoose.Types.ObjectId.isValid(id)){
+    //     res.status(400).json({
+    //         msg:'id is required'
+    //     })
+    // }
+    // try {
+    //     const fsector=await sector.findById(id).populate('MAD').populate('Budget').populate('Payment')
+    //     if(fsector){
+    //         res.json(fsector)
+    //     }else{
+    //         res.json({
+    //             msg:'no sector found'
+    //         })
+    //     }
        
 
         
-    } catch (error) {
-        throw error
-    }
+    // } catch (error) {
+    //     throw error
+    // }
 
 
 }
