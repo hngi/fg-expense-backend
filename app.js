d9d732014f8f670d64f940d13009dbabb3406779
EricoMartin <martinirex@yahoo.co.uk>
2020-06-21 19:52:02 +0100

    heroku error fixes

--- app.js
+++ app.js
@@ -33,6 +33,15 @@ mongoose
   });
 
 // setup middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
