b548e909ad19092ef277570fc07a14481a820dde
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2020-06-23 12:14:43 +0300

    Update projectsController.js

--- controllers/web/projectsController.js
+++ controllers/web/projectsController.js
@@ -1,18 +1,38 @@
-const Project = require("../../models/Project");
+const Expense = require("../../models/expense");
 
-const getAllProjects = async (req, res, next) => {
-  try {
+const getAllProjects = (req, res, next) => 
+{
+/*try {
     let allProjects = await Project.find()
       .populate("company", "_id", "name")
       .populate("mda", "_id", "name")
       .populate("Expense");
 
     return res.json(allProjects);
+*/
+    Expenses.find({})
+    .populate("mdas")
+    .populate("companies")
+    .then( function (expense)
+    {
+      return res.json(
+        {
+          status: "Success",
+          message: "Data retrieved",
+           data: {expense}
+        }
+    }.catch(next);
+
+/*})
   } catch (err) {
     return next(err);
-  }
+  }*/
 };
 
 module.exports = {
   getAllProjects,
 };
+
+
+
+
